e40b2c2722cc4dcb77d643c5610b6b724d48e362
Kristin Barr <kristinbarr@gmail.com>
2019-08-15 23:03:24 -0400

    update picDisaplay comp and styling

--- pic-script/src/components/PicDisplay.js
+++ pic-script/src/components/PicDisplay.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import Loader from 'react-loader-spinner'
 
-const picDisplay = () => {
+import { getPic, getQuote } from '../actions'
+
+const PicDisplay = (props) => {
+  console.log('pic props', props)
+
+  handleClick = (e) => {
+    e.preventDefault()
+    props.getPic()
+    props.getQuote()
+  }
+
 
   return (
-    <div>
-      <h1>Test</h1>
-      {/* <img src=''/> */}
+    <div className='container'>
+      <button onClick={props.getPic}>
+        {props.picIsLoading ? (
+          <Loader type='Triangle' color='#00BFFF' height='20' width='20' />
+        ) : (
+          'NEW PICTURE'
+        )}
+      </button>
+      <h1>PIC SCRIPT</h1>
+      {props.picIsLoading ? (
+        <div> </div>
+      ) : (
+        <img src={props.pic} alt='quote overlay' />
+      )}
     </div>
   )
 }
-export default picDisplay
+
+const mapStateToProps = (state) => {
+  return {
+    pic: state.pic,
+    isLoading: state.isLoading
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { getPic }
+)(PicDisplay)
